407129f7f3ae95223c731961395c51261af78ea0
AB1Battledroid <afeefakif.k@gmail.com>
2025-04-10 09:56:32 +0400

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -1,5 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /Users/seifeldinattia/.zprofile
+echo >> ~/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/seifeldinattia/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
